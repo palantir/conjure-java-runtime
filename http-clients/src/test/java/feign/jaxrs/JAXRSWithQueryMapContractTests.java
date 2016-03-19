83703b56c449031227829d2b19e9804e9cacc9a3
Nick Miyake <nmiyake@palantir.com>
2016-03-19 13:17:27 -0700

    Replace QueryMapAwareContract with JAXRSWithQueryMapContract
    Delegating model made it impossible to support @QueryMap
    annotation on methods that already had body parameters.
    This changes creates a new JAXRSWithQueryMapContract contract
    that replaces the JAXRSContract more directly to allow full
    QueryMap support.

--- http-clients/src/test/java/feign/jaxrs/JAXRSWithQueryMapContractTests.java
+++ http-clients/src/test/java/feign/jaxrs/JAXRSWithQueryMapContractTests.java
@@ -1,24 +1,34 @@
 /*
  * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package com.palantir.remoting.http;
+package feign.jaxrs;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.palantir.remoting.http.FeignClients;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
 import feign.Contract;
 import feign.MethodMetadata;
 import feign.QueryMap;
 import feign.RequestLine;
-import feign.jaxrs.JAXRSContract;
 import java.util.List;
 import java.util.Map;
 import javax.net.ssl.SSLSocketFactory;
@@ -30,15 +40,16 @@ import jersey.repackaged.com.google.common.collect.Iterables;
 import org.junit.Rule;
 import org.junit.Test;
 
-public final class QueryMapAwareContractTests {
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
+public final class JAXRSWithQueryMapContractTests {
 
     @Rule
     public final MockWebServer server = new MockWebServer();
 
     @Test
-    public void testMethodWithQueryMapAndBody() {
+    public void testMethodWithQueryMapAndBodyDefaultContract() {
         // Contract.Default knows how to interpret QueryMap annotation
-        QueryMapAwareContract contract = new QueryMapAwareContract(new Contract.Default());
+        Contract contract = new Contract.Default();
         List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyFeign.class);
 
         assertThat(mdl.size(), is(1));
@@ -46,16 +57,14 @@ public final class QueryMapAwareContractTests {
     }
 
     @Test
-    public void testMethodWithQueryMapAndBodyFails() {
+    public void testMethodWithQueryMapAndBodyJAXRSWithQueryMapContract() {
         // JAXRSContract does not know how to interpret QueryMap annotation; assumes it is a body parameter
-        QueryMapAwareContract contract = new QueryMapAwareContract(new JAXRSContract());
+        JAXRSWithQueryMapContract contract = new JAXRSWithQueryMapContract();
 
-        try {
-            contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyJaxRs.class);
-            fail();
-        } catch (IllegalStateException ex) {
-            assertThat(ex.getMessage(), containsString("Method has too many Body parameters"));
-        }
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyJaxRs.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).queryMapIndex(), is(0));
     }
 
     @Test
