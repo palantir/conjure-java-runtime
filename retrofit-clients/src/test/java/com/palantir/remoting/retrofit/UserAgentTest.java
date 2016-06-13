6d822d68009a6e1732c0384efa95a0fe814e121e
James <jmt158@gmail.com>
2016-06-13 15:24:57 -0700

    Add dash to the allowed user agents (#134)

--- retrofit-clients/src/test/java/com/palantir/remoting/retrofit/UserAgentTest.java
+++ retrofit-clients/src/test/java/com/palantir/remoting/retrofit/UserAgentTest.java
@@ -107,7 +107,7 @@ public final class UserAgentTest {
     @Test
     public void testUserAgent_invalidUserAgentThrows() throws InterruptedException {
         expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage(is("User Agent must match pattern '[A-Za-z0-9()/\\.,_\\s]+': !@"));
+        expectedException.expectMessage(is("User Agent must match pattern '[A-Za-z0-9()-/\\.,_\\s]+': !@"));
 
         OkHttpClientOptions okHttpClientOptions = OkHttpClientOptions.builder().build();
 
