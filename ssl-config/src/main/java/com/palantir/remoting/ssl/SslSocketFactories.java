4d234ed189ce642ecb8685868ce13071ba5d4173
Mark Elliot <mark.elliot@gmail.com>
2016-06-27 09:47:28 -0700

    Add public `createTrustManagers` factory (#146)

--- ssl-config/src/main/java/com/palantir/remoting/ssl/SslSocketFactories.java
+++ ssl-config/src/main/java/com/palantir/remoting/ssl/SslSocketFactories.java
@@ -41,9 +41,7 @@ public final class SslSocketFactories {
      * @return an {@link SSLContext} according to the input configuration
      */
     public static SSLContext createSslContext(SslConfiguration config) {
-        TrustManager[] trustManagers = createTrustManagerFactory(
-                config.trustStorePath(),
-                config.trustStoreType()).getTrustManagers();
+        TrustManager[] trustManagers = createTrustManagers(config);
 
         KeyManager[] keyManagers = null;
         if (config.keyStorePath().isPresent()) {
@@ -61,6 +59,16 @@ public final class SslSocketFactories {
         } catch (GeneralSecurityException e) {
             throw Throwables.propagate(e);
         }
+    }
+
+    /**
+     * Create a {@link TrustManager[]} initialized from the provided configuration.
+     *
+     * @param config an {@link SslConfiguration} describing at least the trust store configuration
+     * @return an {@link TrustManager[]} according to the input configuration
+     */
+    public static TrustManager[] createTrustManagers(SslConfiguration config) {
+        return createTrustManagerFactory(config.trustStorePath(), config.trustStoreType()).getTrustManagers();
     }
 
     private static TrustManagerFactory createTrustManagerFactory(
