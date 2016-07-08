f060f2ef8182bc475615317741e21dcf0f954d99
Aakash Goenka <aakash.goenka@gmail.com>
2016-07-08 19:41:17 +0100

    Update HeaderAccessUtils as per comments

--- http-clients/src/main/java/com/palantir/remoting/http/HeaderAccessUtils.java
+++ http-clients/src/main/java/com/palantir/remoting/http/HeaderAccessUtils.java
@@ -22,27 +22,38 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * Used to access headers in a case-insensitive manner. This is necessary for compatibility with OkHttp 3.3.0+ as
+ * it lower-cases header names whereas we use constants from {@link com.google.common.net.HttpHeaders} where header
+ * names are in Train-Case. This can be removed once {@link feign.Request} and {@link feign.Response} expose the
+ * headers as a map which is case-insensitive with respect to the key. com.netflix.feign:feign-core:8.18.0 will
+ * have it for the {@link feign.Response} headers due to https://github.com/Netflix/feign/pull/418.
+ */
 public final class HeaderAccessUtils {
     private HeaderAccessUtils() {}
 
     public static boolean caseInsensitiveContains(Map<String, Collection<String>> headers, String header) {
-        return caseInsensitiveGet(headers, header) != null;
+        for (String headerName : headers.keySet()) {
+            if (headerName.equalsIgnoreCase(header)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Collection<String> caseInsensitiveGet(Map<String, Collection<String>> headers, String header) {
         return caseInsensitiveMultimapOf(headers).get(header);
     }
 
+    // returns a case-insensitive TreeMap in which the values for all keys equal modulo case are merged.
     private static Map<String, List<String>> caseInsensitiveMultimapOf(Map<String, Collection<String>> headers) {
         Map<String, List<String>> result = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
         for (Map.Entry<String, Collection<String>> entry : headers.entrySet()) {
             String headerName = entry.getKey();
-            List<String> values = result.get(headerName);
-            if (values == null) {
-                values = new LinkedList<>();
-                result.put(headerName, values);
+            if (!result.containsKey(headerName)) {
+                result.put(headerName, new LinkedList<String>());
             }
-            values.addAll(entry.getValue());
+            result.get(headerName).addAll(entry.getValue());
         }
         return result;
     }
