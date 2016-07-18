vaibs96/http-remoting

Add tests for jaxrs-clients

jaxrs-clients/src/test/java/com/palantir/remoting/jaxrs/QueryMapUsingServerWithContextTests.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.remoting.jaxrs;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.palantir.remoting.jaxrs.QueryMapTestServer.TestService;
+import io.dropwizard.Configuration;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import java.util.List;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public final class QueryMapUsingServerWithContextTests {
+
+    /**
+     * Creates a {@link com.palantir.remoting.jaxrs.QueryMapTestServer.WithContext} Dropwizard server.
+     * The Java implementation of this server uses the context to read all
+     * query parameters from a map and returns them.
+     */
+    @ClassRule
+    public static final DropwizardAppRule<Configuration> APP = new DropwizardAppRule<>(
+            com.palantir.remoting.jaxrs.QueryMapTestServer.WithContext.class,
+            "src/test/resources/test-server.yml");
+
+    private com.palantir.remoting.jaxrs.QueryMapTestServer.TestClientService service;
+
+    @Before
+    public void before() {
+        String endpointUri = "http://localhost:" + APP.getLocalPort();
+
+        service = Client.jaxrs().build(QueryMapTestServer.TestClientService.class, "agent", endpointUri);
+    }
+
+    @Test
+    public void testGetUsingParamMapWithQueryMap() {
+        Map<String, String> queryMap = ImmutableMap.of(
+                "fooParam", "fooVal",
+                "barParam", "barVal");
+
+        assertThat(service.getUsingParamMap(queryMap), is(queryMap));
+    }
+
+    @Test
+    public void testGetUsingParamMapWithString() {
+        assertThat(service.getUsingParamMap("fooVal"),
+                is((Map<String, String>) ImmutableMap.of(TestService.MAP_PARAM_NAME, "fooVal")));
+    }
+
+    @Test
+    public void testGetUsingParamListWithQueryMap() {
+        Map<String, List<String>> queryMap = ImmutableMap.<String, List<String>>of(
+                "fooParam", ImmutableList.of("fooVal", "foo value 2", ""),
+                "barParam", ImmutableList.of("barVal"));
+
+        assertThat(service.getUsingParamList(queryMap), is(queryMap));
+    }
+
+    @Test
+    public void testGetUsingParamListWithList() {
+        List<String> paramList = ImmutableList.of("foo", "bar");
+
+        assertThat(
+                service.getUsingParamList(paramList),
+                is((Map<String, List<String>>) ImmutableMap.of(
+                        TestService.LIST_PARAM_NAME, paramList)));
+    }
+
+}

jaxrs-clients/src/test/java/com/palantir/remoting/jaxrs/QueryMapUsingServerWithoutContextTests.java
@@ -1,0 +1,99 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.remoting.jaxrs;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.palantir.remoting.jaxrs.QueryMapTestServer.TestService;
+import io.dropwizard.Configuration;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import java.util.List;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public final class QueryMapUsingServerWithoutContextTests {
+
+    /**
+     * Creates a {@link com.palantir.remoting.jaxrs.QueryMapTestServer.WithoutContext} Dropwizard server.
+     * The Java implementation of this server uses strongly typed methods that
+     * will only process the query parameters that are declared as part of the
+     * method parameters.
+     */
+    @ClassRule
+    public static final DropwizardAppRule<Configuration> APP = new DropwizardAppRule<>(
+            com.palantir.remoting.jaxrs.QueryMapTestServer.WithoutContext.class,
+            "src/test/resources/test-server.yml");
+
+    private com.palantir.remoting.jaxrs.QueryMapTestServer.TestClientService service;
+
+    @Before
+    public void before() {
+        String endpointUri = "http://localhost:" + APP.getLocalPort();
+        service = Client.jaxrs().build(QueryMapTestServer.TestClientService.class, "agent", endpointUri);
+    }
+
+    @Test
+    public void testGetUsingParamMapWithQueryMap() {
+        Map<String, String> queryMap = ImmutableMap.of(
+                TestService.MAP_PARAM_NAME, "namedVal 1",
+                "fooParam", "fooVal");
+
+        // client GET call contains all of the query parameters in queryMap,
+        // but Java server implementation is such that only first value of
+        // TestService.MAP_PARAM_NAME query parameter is processed
+        assertThat(
+                service.getUsingParamMap(queryMap),
+                is((Map<String, String>) ImmutableMap.of(TestService.MAP_PARAM_NAME, "namedVal 1")));
+    }
+
+    @Test
+    public void testGetUsingParamMapWithString() {
+        assertThat(
+                service.getUsingParamMap("fooVal"),
+                is((Map<String, String>) ImmutableMap.of(TestService.MAP_PARAM_NAME, "fooVal")));
+    }
+
+    @Test
+    public void testGetUsingParamListWithQueryMap() {
+        Map<String, List<String>> queryMap = ImmutableMap.<String, List<String>>of(
+                TestService.LIST_PARAM_NAME, ImmutableList.of("list val 1", "list val 2"),
+                "fooParam", ImmutableList.of("foo value"),
+                TestService.MAP_PARAM_NAME, ImmutableList.of("bar value"));
+
+        // client GET call contains all of the query parameters in queryMap,
+        // but Java server implementation is such that only values of
+        // TestService.LIST_PARAM_NAME query parameter are processed
+        assertThat(
+                service.getUsingParamList(queryMap),
+                is((Map<String, List<String>>) ImmutableMap.<String, List<String>>of(
+                        TestService.LIST_PARAM_NAME, ImmutableList.of("list val 1", "list val 2"))));
+    }
+
+    @Test
+    public void testGetUsingParamListWithList() {
+        assertThat(
+                service.getUsingParamList(ImmutableList.of("foo", "bar")),
+                is((Map<String, List<String>>) ImmutableMap.<String, List<String>>of(
+                        TestService.LIST_PARAM_NAME, ImmutableList.of("foo", "bar"))));
+    }
+
+}

jaxrs-clients/src/test/java/feign/jaxrs/JaxRsWithHeaderAndQueryMapContractTests.java
@@ -1,0 +1,176 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package feign.jaxrs;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.palantir.remoting.jaxrs.Client;
+import feign.Contract;
+import feign.HeaderMap;
+import feign.MethodMetadata;
+import feign.QueryMap;
+import feign.RequestLine;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.Rule;
+import org.junit.Test;
+
+public final class JaxRsWithHeaderAndQueryMapContractTests {
+
+    @Rule
+    public final MockWebServer server = new MockWebServer();
+
+    @Test
+    public void testMethodWithHeaderMapAndBodyDefaultContract() {
+        // Contract.Default knows how to interpret HeaderMap annotation
+        Contract contract = new Contract.Default();
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(HeaderMapTestInterfaceWithBodyFeign.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).headerMapIndex(), is(0));
+    }
+
+    @Test
+    public void testMethodWithHeaderMapAndBodyJaxRsContractFails() {
+        // JAXRSContract does not know how to interpret HeaderMap annotation; assumes it is a body parameter
+        Contract contract = new JAXRSContract();
+
+        try {
+            contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyJaxRs.class);
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), containsString("Method has too many Body parameters"));
+        }
+    }
+
+    @Test
+    public void testMethodWithHeaderMapAndBodyJaxRsWithHeaderAndQueryMapContract() {
+        JaxRsWithHeaderAndQueryMapContract contract = new JaxRsWithHeaderAndQueryMapContract();
+
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(HeaderMapTestInterfaceWithBodyJaxRs.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).headerMapIndex(), is(0));
+    }
+
+    @Test
+    public void testJaxRsInterfaceWithHeaderMap() throws Exception {
+        HeaderMapTestInterface proxy = Client.jaxrs()
+                .build(HeaderMapTestInterface.class, "agent", "http://localhost:" + server.getPort());
+        server.enqueue(new MockResponse());
+
+        proxy.header("alice", ImmutableMap.of("fooKey", "fooValue"));
+
+        RecordedRequest recordedRequest = server.takeRequest();
+        assertThat(recordedRequest.getHeader("name"), is("alice"));
+        assertThat(recordedRequest.getHeader("fooKey"), is("fooValue"));
+    }
+
+    @Test
+    public void testMethodWithQueryMapAndBodyDefaultContract() {
+        // Contract.Default knows how to interpret QueryMap annotation
+        Contract contract = new Contract.Default();
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyFeign.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).queryMapIndex(), is(0));
+    }
+
+    @Test
+    public void testMethodWithQueryMapAndBodyJaxRsContractFails() {
+        // JAXRSContract does not know how to interpret HeaderMap annotation; assumes it is a body parameter
+        Contract contract = new JAXRSContract();
+
+        try {
+            contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyJaxRs.class);
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), containsString("Method has too many Body parameters"));
+        }
+    }
+
+    @Test
+    public void testMethodWithQueryMapAndBodyJaxRsWithHeaderAndQueryMapContract() {
+        JaxRsWithHeaderAndQueryMapContract contract = new JaxRsWithHeaderAndQueryMapContract();
+
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyJaxRs.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).queryMapIndex(), is(0));
+    }
+
+    @Test
+    public void testJaxRsInterfaceWithQueryMap() throws Exception {
+        QueryMapTestInterface proxy = Client.jaxrs()
+                .build(QueryMapTestInterface.class, "agent", "http://localhost:" + server.getPort());
+        server.enqueue(new MockResponse());
+
+        proxy.query("alice", ImmutableMap.of("fooKey", "fooValue"));
+
+        assertThat(server.takeRequest().getPath(), is("/query?name=alice&fooKey=fooValue"));
+    }
+
+    @Path("/")
+    private interface HeaderMapTestInterface {
+        @GET
+        @Path("header")
+        String header(@HeaderParam("name") String name, @HeaderMap Map<String, String> headerMap);
+    }
+
+    @Path("/")
+    private interface HeaderMapTestInterfaceWithBodyJaxRs {
+        @GET
+        @Path("header")
+        String header(@HeaderMap Map<String, String> headerMap, String body);
+    }
+
+    private interface HeaderMapTestInterfaceWithBodyFeign {
+        @PUT
+        @RequestLine("PUT /bar")
+        String header(@HeaderMap Map<String, String> headerMap, String body);
+    }
+
+    @Path("/")
+    private interface QueryMapTestInterface {
+        @GET
+        @Path("query")
+        String query(@QueryParam("name") String name, @QueryMap Map<String, String> queryMap);
+    }
+
+    @Path("/")
+    private interface QueryMapTestInterfaceWithBodyJaxRs {
+        @PUT
+        @Path("bar")
+        void queryWithBody(@QueryMap Map<String, String> queryMap, String body);
+    }
+
+    private interface QueryMapTestInterfaceWithBodyFeign {
+        @PUT
+        @RequestLine("PUT /bar")
+        void queryWithBody(@QueryMap Map<String, String> queryMap, String body);
+    }
+}

palantir/conjure-java-runtime

jaxrs-clients/src/test/java/com/palantir/remoting/jaxrs/QueryMapUsingServerWithContextTests.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.remoting.jaxrs;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.palantir.remoting.jaxrs.QueryMapTestServer.TestService;
+import io.dropwizard.Configuration;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import java.util.List;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public final class QueryMapUsingServerWithContextTests {
+
+    /**
+     * Creates a {@link com.palantir.remoting.jaxrs.QueryMapTestServer.WithContext} Dropwizard server.
+     * The Java implementation of this server uses the context to read all
+     * query parameters from a map and returns them.
+     */
+    @ClassRule
+    public static final DropwizardAppRule<Configuration> APP = new DropwizardAppRule<>(
+            com.palantir.remoting.jaxrs.QueryMapTestServer.WithContext.class,
+            "src/test/resources/test-server.yml");
+
+    private com.palantir.remoting.jaxrs.QueryMapTestServer.TestClientService service;
+
+    @Before
+    public void before() {
+        String endpointUri = "http://localhost:" + APP.getLocalPort();
+
+        service = Client.builder().build(QueryMapTestServer.TestClientService.class, "agent", endpointUri);
+    }
+
+    @Test
+    public void testGetUsingParamMapWithQueryMap() {
+        Map<String, String> queryMap = ImmutableMap.of(
+                "fooParam", "fooVal",
+                "barParam", "barVal");
+
+        assertThat(service.getUsingParamMap(queryMap), is(queryMap));
+    }
+
+    @Test
+    public void testGetUsingParamMapWithString() {
+        assertThat(service.getUsingParamMap("fooVal"),
+                is((Map<String, String>) ImmutableMap.of(TestService.MAP_PARAM_NAME, "fooVal")));
+    }
+
+    @Test
+    public void testGetUsingParamListWithQueryMap() {
+        Map<String, List<String>> queryMap = ImmutableMap.<String, List<String>>of(
+                "fooParam", ImmutableList.of("fooVal", "foo value 2", ""),
+                "barParam", ImmutableList.of("barVal"));
+
+        assertThat(service.getUsingParamList(queryMap), is(queryMap));
+    }
+
+    @Test
+    public void testGetUsingParamListWithList() {
+        List<String> paramList = ImmutableList.of("foo", "bar");
+
+        assertThat(
+                service.getUsingParamList(paramList),
+                is((Map<String, List<String>>) ImmutableMap.of(
+                        TestService.LIST_PARAM_NAME, paramList)));
+    }
+
+}

jaxrs-clients/src/test/java/com/palantir/remoting/jaxrs/QueryMapUsingServerWithoutContextTests.java
@@ -1,0 +1,99 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.remoting.jaxrs;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.palantir.remoting.jaxrs.QueryMapTestServer.TestService;
+import io.dropwizard.Configuration;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import java.util.List;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public final class QueryMapUsingServerWithoutContextTests {
+
+    /**
+     * Creates a {@link com.palantir.remoting.jaxrs.QueryMapTestServer.WithoutContext} Dropwizard server.
+     * The Java implementation of this server uses strongly typed methods that
+     * will only process the query parameters that are declared as part of the
+     * method parameters.
+     */
+    @ClassRule
+    public static final DropwizardAppRule<Configuration> APP = new DropwizardAppRule<>(
+            com.palantir.remoting.jaxrs.QueryMapTestServer.WithoutContext.class,
+            "src/test/resources/test-server.yml");
+
+    private com.palantir.remoting.jaxrs.QueryMapTestServer.TestClientService service;
+
+    @Before
+    public void before() {
+        String endpointUri = "http://localhost:" + APP.getLocalPort();
+        service = Client.builder().build(QueryMapTestServer.TestClientService.class, "agent", endpointUri);
+    }
+
+    @Test
+    public void testGetUsingParamMapWithQueryMap() {
+        Map<String, String> queryMap = ImmutableMap.of(
+                TestService.MAP_PARAM_NAME, "namedVal 1",
+                "fooParam", "fooVal");
+
+        // client GET call contains all of the query parameters in queryMap,
+        // but Java server implementation is such that only first value of
+        // TestService.MAP_PARAM_NAME query parameter is processed
+        assertThat(
+                service.getUsingParamMap(queryMap),
+                is((Map<String, String>) ImmutableMap.of(TestService.MAP_PARAM_NAME, "namedVal 1")));
+    }
+
+    @Test
+    public void testGetUsingParamMapWithString() {
+        assertThat(
+                service.getUsingParamMap("fooVal"),
+                is((Map<String, String>) ImmutableMap.of(TestService.MAP_PARAM_NAME, "fooVal")));
+    }
+
+    @Test
+    public void testGetUsingParamListWithQueryMap() {
+        Map<String, List<String>> queryMap = ImmutableMap.<String, List<String>>of(
+                TestService.LIST_PARAM_NAME, ImmutableList.of("list val 1", "list val 2"),
+                "fooParam", ImmutableList.of("foo value"),
+                TestService.MAP_PARAM_NAME, ImmutableList.of("bar value"));
+
+        // client GET call contains all of the query parameters in queryMap,
+        // but Java server implementation is such that only values of
+        // TestService.LIST_PARAM_NAME query parameter are processed
+        assertThat(
+                service.getUsingParamList(queryMap),
+                is((Map<String, List<String>>) ImmutableMap.<String, List<String>>of(
+                        TestService.LIST_PARAM_NAME, ImmutableList.of("list val 1", "list val 2"))));
+    }
+
+    @Test
+    public void testGetUsingParamListWithList() {
+        assertThat(
+                service.getUsingParamList(ImmutableList.of("foo", "bar")),
+                is((Map<String, List<String>>) ImmutableMap.<String, List<String>>of(
+                        TestService.LIST_PARAM_NAME, ImmutableList.of("foo", "bar"))));
+    }
+
+}

jaxrs-clients/src/test/java/feign/jaxrs/JaxRsWithHeaderAndQueryMapContractTests.java
@@ -1,0 +1,176 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package feign.jaxrs;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.palantir.remoting.jaxrs.Client;
+import feign.Contract;
+import feign.HeaderMap;
+import feign.MethodMetadata;
+import feign.QueryMap;
+import feign.RequestLine;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.Rule;
+import org.junit.Test;
+
+public final class JaxRsWithHeaderAndQueryMapContractTests {
+
+    @Rule
+    public final MockWebServer server = new MockWebServer();
+
+    @Test
+    public void testMethodWithHeaderMapAndBodyDefaultContract() {
+        // Contract.Default knows how to interpret HeaderMap annotation
+        Contract contract = new Contract.Default();
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(HeaderMapTestInterfaceWithBodyFeign.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).headerMapIndex(), is(0));
+    }
+
+    @Test
+    public void testMethodWithHeaderMapAndBodyJaxRsContractFails() {
+        // JAXRSContract does not know how to interpret HeaderMap annotation; assumes it is a body parameter
+        Contract contract = new JAXRSContract();
+
+        try {
+            contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyJaxRs.class);
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), containsString("Method has too many Body parameters"));
+        }
+    }
+
+    @Test
+    public void testMethodWithHeaderMapAndBodyJaxRsWithHeaderAndQueryMapContract() {
+        JaxRsWithHeaderAndQueryMapContract contract = new JaxRsWithHeaderAndQueryMapContract();
+
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(HeaderMapTestInterfaceWithBodyJaxRs.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).headerMapIndex(), is(0));
+    }
+
+    @Test
+    public void testJaxRsInterfaceWithHeaderMap() throws Exception {
+        HeaderMapTestInterface proxy = Client.builder()
+                .build(HeaderMapTestInterface.class, "agent", "http://localhost:" + server.getPort());
+        server.enqueue(new MockResponse());
+
+        proxy.header("alice", ImmutableMap.of("fooKey", "fooValue"));
+
+        RecordedRequest recordedRequest = server.takeRequest();
+        assertThat(recordedRequest.getHeader("name"), is("alice"));
+        assertThat(recordedRequest.getHeader("fooKey"), is("fooValue"));
+    }
+
+    @Test
+    public void testMethodWithQueryMapAndBodyDefaultContract() {
+        // Contract.Default knows how to interpret QueryMap annotation
+        Contract contract = new Contract.Default();
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyFeign.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).queryMapIndex(), is(0));
+    }
+
+    @Test
+    public void testMethodWithQueryMapAndBodyJaxRsContractFails() {
+        // JAXRSContract does not know how to interpret HeaderMap annotation; assumes it is a body parameter
+        Contract contract = new JAXRSContract();
+
+        try {
+            contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyJaxRs.class);
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(), containsString("Method has too many Body parameters"));
+        }
+    }
+
+    @Test
+    public void testMethodWithQueryMapAndBodyJaxRsWithHeaderAndQueryMapContract() {
+        JaxRsWithHeaderAndQueryMapContract contract = new JaxRsWithHeaderAndQueryMapContract();
+
+        List<MethodMetadata> mdl = contract.parseAndValidatateMetadata(QueryMapTestInterfaceWithBodyJaxRs.class);
+
+        assertThat(mdl.size(), is(1));
+        assertThat(Iterables.getOnlyElement(mdl).queryMapIndex(), is(0));
+    }
+
+    @Test
+    public void testJaxRsInterfaceWithQueryMap() throws Exception {
+        QueryMapTestInterface proxy = Client.builder()
+                .build(QueryMapTestInterface.class, "agent", "http://localhost:" + server.getPort());
+        server.enqueue(new MockResponse());
+
+        proxy.query("alice", ImmutableMap.of("fooKey", "fooValue"));
+
+        assertThat(server.takeRequest().getPath(), is("/query?name=alice&fooKey=fooValue"));
+    }
+
+    @Path("/")
+    private interface HeaderMapTestInterface {
+        @GET
+        @Path("header")
+        String header(@HeaderParam("name") String name, @HeaderMap Map<String, String> headerMap);
+    }
+
+    @Path("/")
+    private interface HeaderMapTestInterfaceWithBodyJaxRs {
+        @GET
+        @Path("header")
+        String header(@HeaderMap Map<String, String> headerMap, String body);
+    }
+
+    private interface HeaderMapTestInterfaceWithBodyFeign {
+        @PUT
+        @RequestLine("PUT /bar")
+        String header(@HeaderMap Map<String, String> headerMap, String body);
+    }
+
+    @Path("/")
+    private interface QueryMapTestInterface {
+        @GET
+        @Path("query")
+        String query(@QueryParam("name") String name, @QueryMap Map<String, String> queryMap);
+    }
+
+    @Path("/")
+    private interface QueryMapTestInterfaceWithBodyJaxRs {
+        @PUT
+        @Path("bar")
+        void queryWithBody(@QueryMap Map<String, String> queryMap, String body);
+    }
+
+    private interface QueryMapTestInterfaceWithBodyFeign {
+        @PUT
+        @RequestLine("PUT /bar")
+        void queryWithBody(@QueryMap Map<String, String> queryMap, String body);
+    }
+}