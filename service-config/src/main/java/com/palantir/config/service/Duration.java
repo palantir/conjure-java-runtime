a1d78ba6938d824f31cd544fe3dfce4c35deb031
Robert Fink <rfink@palantir.com>
2016-07-25 16:53:54 -0700

    fix copyright in Duration.java

--- service-config/src/main/java/com/palantir/config/service/Duration.java
+++ service-config/src/main/java/com/palantir/config/service/Duration.java
@@ -1,6 +1,18 @@
 /*
  * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * Included from Dropwizard's https://github.com/dropwizard/dropwizard/blob/b9e9e835fd5be10180a839a3f38ca93b9c8e0652/dropwizard-util/src/main/java/io/dropwizard/util/Duration.java
  * under Apache2 license.
  */
