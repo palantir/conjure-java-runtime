197ea42243f6dcfa79cb6d3bb5e5efac853b6514
Robert Fink <rf@robertfink.de>
2016-10-12 17:26:48 -0700

    Add docs and tests for Traces class (#256)

---
+++ tracing/src/test/java/com/palantir/remoting1/tracing/TracesTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.remoting1.tracing;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public final class TracesTest {
+
+    @Mock
+    private Traces.Subscriber subscriber1;
+    @Mock
+    private Traces.Subscriber subscriber2;
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSubscribeUnsubscribe() throws Exception {
+        // no error when completing span without a registered subscriber
+        startAndCompleteSpan();
+
+        Traces.subscribe(subscriber1);
+        Traces.subscribe(subscriber2);
+        Span span = startAndCompleteSpan();
+        verify(subscriber1).consume(span);
+        verify(subscriber2).consume(span);
+        verifyNoMoreInteractions(subscriber1, subscriber2);
+
+        Traces.unsubscribe(subscriber1);
+        span = startAndCompleteSpan();
+        verify(subscriber2).consume(span);
+        verifyNoMoreInteractions(subscriber1, subscriber2);
+
+        Traces.unsubscribe(subscriber2);
+        startAndCompleteSpan();
+        verifyNoMoreInteractions(subscriber1, subscriber2);
+    }
+
+    @Test
+    public void testSubscribingIsIdempontent() throws Exception {
+        Traces.subscribe(subscriber1);
+        Traces.subscribe(subscriber1);
+        Span span = startAndCompleteSpan();
+        verify(subscriber1, times(1)).consume(span);
+        verifyNoMoreInteractions(subscriber1, subscriber2);
+    }
+
+    @Test
+    public void testDoesNotNotifySubscribersWhenCompletingNonexistingSpan() throws Exception {
+        Traces.subscribe(subscriber1);
+        Traces.subscribe(subscriber2);
+        Traces.completeSpan(); // no active span.
+        verifyNoMoreInteractions(subscriber1, subscriber2);
+    }
+
+    private static Span startAndCompleteSpan() {
+        Traces.startSpan("operation");
+        return Traces.completeSpan().get();
+    }
+}
