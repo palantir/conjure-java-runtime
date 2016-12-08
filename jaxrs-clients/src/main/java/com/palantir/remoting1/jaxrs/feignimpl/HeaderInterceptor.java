95af5fb8420c3dc935a2240baa9643ce13c7d6da
Alessandra Loro <aloro@palantir.com>
2016-12-08 11:05:02 +1100

    Fail on null headers, warn in case of overwriting existing headers

--- jaxrs-clients/src/main/java/com/palantir/remoting1/jaxrs/feignimpl/HeaderInterceptor.java
+++ jaxrs-clients/src/main/java/com/palantir/remoting1/jaxrs/feignimpl/HeaderInterceptor.java
@@ -16,18 +16,23 @@
 
 package com.palantir.remoting1.jaxrs.feignimpl;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import java.util.Collections;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class HeaderInterceptor implements RequestInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(HeaderInterceptor.class);
 
     private final Map<String, String> headers;
 
     private HeaderInterceptor(Map<String, String> headers) {
-        this.headers = (headers != null) ? ImmutableMap.copyOf(headers) : Collections.<String, String>emptyMap();
+        this.headers = Preconditions.checkNotNull(ImmutableMap.copyOf(headers),
+                "Custom request headers cannot be null");
     }
 
     public static HeaderInterceptor of(Map<String, String> headers) {
@@ -37,6 +42,10 @@ public final class HeaderInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
         for (Map.Entry<String, String> header : headers.entrySet()) {
+            if (template.headers().containsKey(header.getKey())) {
+                log.warn("Header {} with value {} is being overwritten with value {}", header.getKey(),
+                        template.headers().get(header.getKey()), header.getValue());
+            }
             template.header(header.getKey(), header.getValue());
         }
     }
