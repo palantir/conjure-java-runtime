ccbdba7f0d427fd994b163467741947885eba4c7
dlisuk <dlisuk@palantir.com>
2017-01-06 13:18:18 -0500

    Adding jdk8 support to the object mapper

--- extras/jackson-support/src/main/java/com/palantir/remoting1/ext/jackson/ObjectMappers.java
+++ extras/jackson-support/src/main/java/com/palantir/remoting1/ext/jackson/ObjectMappers.java
@@ -19,6 +19,7 @@ package com.palantir.remoting1.ext.jackson;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 
 public final class ObjectMappers {
@@ -29,11 +30,12 @@ public final class ObjectMappers {
      * Returns a newly allocated {@link ObjectMapper} that is configured with the Guava module and the JDK 7 module
      * (if present).
      */
-    public static ObjectMapper guavaJdk7() {
+    public static ObjectMapper guavaJdk8() {
         // TODO: Replace this code with shading to support different versions of Jackson
         ObjectMapper mapper = new ObjectMapper()
                 .registerModule(new GuavaModule())
-                .registerModule(new ShimJdk7Module());
+                .registerModule(new ShimJdk7Module())
+                .registerModule(new Jdk8Module());
         if (hasJackson25()) {
             // Afterburner on Jackson 2.4 will throw
             // java.lang.NoSuchMethodError: com.fasterxml.jackson.core.JsonParser.isExpectedStartObjectToken()Z
