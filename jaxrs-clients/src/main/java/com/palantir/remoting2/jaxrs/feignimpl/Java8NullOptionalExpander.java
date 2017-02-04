917d7b0fa0c18401311e602212a655511c1395fb
Callum Rogers <callumr@palantir.com>
2017-02-03 16:45:29 -0800

    Unify OptionalInt and Optional

--- jaxrs-clients/src/main/java/com/palantir/remoting2/jaxrs/feignimpl/Java8NullOptionalExpander.java
+++ jaxrs-clients/src/main/java/com/palantir/remoting2/jaxrs/feignimpl/Java8NullOptionalExpander.java
@@ -17,20 +17,88 @@
 package com.palantir.remoting2.jaxrs.feignimpl;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import feign.Param.Expander;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 /**
  * Expands Optional by using null for {@link Optional#empty()} and the {@link Object#toString()} of the
  * value otherwise.
  */
 public final class Java8NullOptionalExpander implements Expander {
+    private static final List<OptionalLike> OPTIONAL_LIKES = ImmutableList.of(
+            new NormalOptionalLike(),
+            new OptionalIntLike()
+    );
+
+    interface OptionalLike<T> {
+        Class<?> clazz();
+        boolean isPresent(T value);
+        String get(T value);
+
+        default boolean isAssignableFrom(Object obj) {
+            return clazz().isAssignableFrom(obj.getClass());
+        }
+
+        default String asNullableString(T value) {
+            return isPresent(value) ? asNullableString(value) : null;
+        }
+    }
 
     @Override
     public String expand(Object value) {
-        Preconditions.checkArgument(value instanceof Optional, "Value must be an Optional. Was: %s", value.getClass());
-        Optional<?> optional = (Optional<?>) value;
-        return optional.isPresent() ? Objects.toString(optional.get()) : null;
+        Preconditions.checkArgument(value instanceof OptionalInt, "Value must be an Optional. Was: %s", value.getClass());
+
+        return OPTIONAL_LIKES.stream()
+                .filter(optionalLike -> optionalLike.isAssignableFrom(value))
+                .findFirst()
+                .map((optionalLike) -> optionalLike.asNullableString(value))
+                .orElseThrow(() -> {
+                    List<Class<?>> clazzes = OPTIONAL_LIKES.stream()
+                            .map(OptionalLike::clazz)
+                            .collect(Collectors.toList());
+
+                    return new RuntimeException(String.format("Value must be one of type %s. Was: %s", clazzes, value.getClass()));
+                });
     }
+
+    private static class NormalOptionalLike implements OptionalLike<Optional> {
+        @Override
+        public Class<?> clazz() {
+            return Optional.class;
+        }
+
+        @Override
+        public boolean isPresent(Optional value) {
+            return value.isPresent();
+        }
+
+        @Override
+        public String get(Optional value) {
+            return Objects.toString(value.get());
+        }
+    }
+
+    private static class OptionalIntLike implements OptionalLike<OptionalInt> {
+        @Override
+        public Class<?> clazz() {
+            return OptionalInt.class;
+        }
+
+        @Override
+        public boolean isPresent(OptionalInt value) {
+            return value.isPresent();
+        }
+
+        @Override
+        public String get(OptionalInt value) {
+            return Integer.toString(value.getAsInt());
+        }
+    }
+
+
 }
