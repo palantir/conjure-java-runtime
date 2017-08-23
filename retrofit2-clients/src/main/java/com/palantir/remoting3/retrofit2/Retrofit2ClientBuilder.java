54e31a901ac298c1000e527cafd10bd93680b7ad
Robert Fink <rfink@palantir.com>
2017-08-23 16:26:05 +0200

    Construct OkHttp clients (mostly) in one place (#515)

--- retrofit2-clients/src/main/java/com/palantir/remoting3/retrofit2/Retrofit2ClientBuilder.java
+++ retrofit2-clients/src/main/java/com/palantir/remoting3/retrofit2/Retrofit2ClientBuilder.java
@@ -18,37 +18,19 @@ package com.palantir.remoting3.retrofit2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-import com.google.common.net.HttpHeaders;
-import com.palantir.remoting.api.config.service.BasicCredentials;
-import com.palantir.remoting3.clients.CipherSuites;
 import com.palantir.remoting3.clients.ClientConfiguration;
 import com.palantir.remoting3.ext.jackson.ObjectMappers;
 import com.palantir.remoting3.okhttp.AsyncCallTagCallFactory;
-import com.palantir.remoting3.okhttp.MultiServerRetryInterceptor;
-import com.palantir.remoting3.okhttp.OkhttpSlf4jDebugLogger;
+import com.palantir.remoting3.okhttp.OkHttpClients;
 import com.palantir.remoting3.okhttp.RetryInterceptor;
 import com.palantir.remoting3.okhttp.SerializableErrorInterceptor;
-import com.palantir.remoting3.okhttp.UserAgentInterceptor;
-import com.palantir.remoting3.tracing.Tracers;
-import com.palantir.remoting3.tracing.okhttp3.OkhttpTraceInterceptor;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
-import okhttp3.ConnectionPool;
-import okhttp3.ConnectionSpec;
-import okhttp3.Credentials;
-import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
-import okhttp3.TlsVersion;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public final class Retrofit2ClientBuilder {
-    private static final ExecutorService executorService = Tracers.wrap(new Dispatcher().executorService());
-
     private static final ObjectMapper CBOR_OBJECT_MAPPER = ObjectMappers.newCborClientObjectMapper();
     private static final ObjectMapper OBJECT_MAPPER = ObjectMappers.newClientObjectMapper();
 
@@ -80,69 +62,17 @@ public final class Retrofit2ClientBuilder {
     }
 
     private OkHttpClient createOkHttpClient(String userAgent, List<String> uris) {
-        OkHttpClient.Builder client = new OkHttpClient.Builder();
-
-        // SSL
-        client.sslSocketFactory(config.sslSocketFactory(), config.trustManager());
-
-        // proxy
-        client.proxySelector(config.proxy());
-        if (config.proxyCredentials().isPresent()) {
-            BasicCredentials basicCreds = config.proxyCredentials().get();
-            final String credentials = Credentials.basic(basicCreds.username(), basicCreds.password());
-            client.proxyAuthenticator((route, response) -> response.request().newBuilder()
-                    .header(HttpHeaders.PROXY_AUTHORIZATION, credentials)
-                    .build());
-        }
-
-        // tracing
-        client.interceptors().add(OkhttpTraceInterceptor.INSTANCE);
-
-        // timeouts
-        client.connectTimeout(config.connectTimeout().toMillis(), TimeUnit.MILLISECONDS);
-        client.readTimeout(config.readTimeout().toMillis(), TimeUnit.MILLISECONDS);
-        client.writeTimeout(config.writeTimeout().toMillis(), TimeUnit.MILLISECONDS);
+        OkHttpClient.Builder client = OkHttpClients.builder(config, userAgent);
 
         // retry configuration
+        // TODO(rfink): Consolidate this with the MultiServerRetry thing.
         if (config.maxNumRetries() > 1) {
             client.addInterceptor(new RetryInterceptor(config.maxNumRetries()));
         }
 
-        client.addInterceptor(MultiServerRetryInterceptor.create(uris));
-        client.addInterceptor(UserAgentInterceptor.of(userAgent));
+        // TODO(rfink): Can we use the interceptor to handle errors in Feign clients?
         client.addInterceptor(SerializableErrorInterceptor.INSTANCE);
 
-        // cipher setup
-        client.connectionSpecs(createConnectionSpecs(config.enableGcmCipherSuites()));
-
-        // increase default connection pool from 5 @ 5 minutes to 100 @ 10 minutes
-        client.connectionPool(new ConnectionPool(100, 10, TimeUnit.MINUTES));
-
-        client.dispatcher(createDispatcher());
-
-        // logging
-        client.addInterceptor(new HttpLoggingInterceptor(OkhttpSlf4jDebugLogger.INSTANCE));
-
         return client.build();
-    }
-
-    private static ImmutableList<ConnectionSpec> createConnectionSpecs(boolean enableGcmCipherSuites) {
-        return ImmutableList.of(
-                new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                        .tlsVersions(TlsVersion.TLS_1_2)
-                        .cipherSuites(enableGcmCipherSuites
-                                ? CipherSuites.allCipherSuites()
-                                : CipherSuites.fastCipherSuites())
-                        .build(),
-                ConnectionSpec.CLEARTEXT);
-    }
-
-    private static Dispatcher createDispatcher() {
-        Dispatcher dispatcher = new Dispatcher(executorService);
-
-        dispatcher.setMaxRequests(256);
-        dispatcher.setMaxRequestsPerHost(256);
-
-        return dispatcher;
     }
 }
