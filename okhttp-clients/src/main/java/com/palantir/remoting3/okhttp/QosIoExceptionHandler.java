394061dcbe870372775d8fa500ad9d2092eeef07
Robert Fink <rfink@palantir.com>
2017-08-31 13:40:38 +0200

    add back javadoc in QosIoExceptionHandler

--- okhttp-clients/src/main/java/com/palantir/remoting3/okhttp/QosIoExceptionHandler.java
+++ okhttp-clients/src/main/java/com/palantir/remoting3/okhttp/QosIoExceptionHandler.java
@@ -19,13 +19,29 @@ package com.palantir.remoting3.okhttp;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.palantir.remoting.api.errors.QosException;
 import okhttp3.Call;
+import okhttp3.Interceptor;
 import okhttp3.Response;
 
 interface QosIoExceptionHandler {
-    /**
+     /**
      * Handles a {@link QosIoException} that was thrown for the given {@link Call} and returns a future for a suitable
      * response. The future can either encapsulate the original exception, or the response to retrying the call at a
      * later time, or the exception thrown by a later retried execution.
+     * <p>
+     * Note that vanilla OkHttp functionality does not cover what's needed here, for example:
+     * <ul>
+     *     <li>
+     *         OkHttp doesn't have out-of-the-box support for re-locating complex requests,
+     *         cf. https://github.com/square/okhttp/issues/3111 .
+     *      </li>
+     *      <li>
+     *          OkHttp {@link Interceptor}s could wait-and-retry upon observing 429 or 503, but this would happen on the
+     *          same thread. Since all http-remoting clients (in a JVM) share the same OkHttp thread pool, only a few
+     *          backed up requests would have the potential to stall all outgoing RPC. The {@link QosIoExceptionHandler}
+     *          approach taken here circumnavigates this pitful by re-scheduling call re-execution instead of performing
+     *          thread-blocking sleep.
+     *      </li>
+     * </ul>
      * <p>
      * The end-to-end flow for handling {@link QosException}s in an OkHttp client is as follows:
      * <ul>
