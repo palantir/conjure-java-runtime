96d6be33ca54393ba62106f208b80a64d6a49e60
James Baker <jbaker@palantir.com>
2017-10-24 13:55:50 +0100

    Checkstyle

--- jaxrs-clients/src/main/java/com/palantir/remoting3/jaxrs/feignimpl/PathTemplateHeaderRewriter.java
+++ jaxrs-clients/src/main/java/com/palantir/remoting3/jaxrs/feignimpl/PathTemplateHeaderRewriter.java
@@ -21,7 +21,6 @@ import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
