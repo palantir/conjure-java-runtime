fc5b9898521d391c78c7feaa97d3a577f4c26680
Dan Sanduleac <dsanduleac@palantir.com>
2018-05-03 14:46:12 +0100

    Implement a static cache pool

--- http-clients/src/main/java/com/palantir/remoting3/clients/CacheConfig.java
+++ http-clients/src/main/java/com/palantir/remoting3/clients/CacheConfig.java
@@ -4,11 +4,15 @@
 
 package com.palantir.remoting3.clients;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.io.File;
 import org.immutables.value.Value;
 
 @Value.Immutable
 @ImmutablesStyle
+@JsonSerialize(as = ImmutableCacheConfig.class)
+@JsonDeserialize(as = ImmutableCacheConfig.class)
 public interface CacheConfig {
 
     File directory();
