a7fc9eff782f376c90bcc57af23610d5449b8abb
Dan Sănduleac <dansanduleac@users.noreply.github.com>
2018-05-15 18:09:46 +0100

    Shared dispatcher and connection pool (#706)

--- okhttp-clients/src/main/java/com/palantir/remoting3/okhttp/OkHttpClients.java
+++ okhttp-clients/src/main/java/com/palantir/remoting3/okhttp/OkHttpClients.java
@@ -54,6 +54,18 @@ public final class OkHttpClients {
      */
     private static final ExecutorService executionExecutor = Tracers.wrap(new Dispatcher().executorService());
 
+    /** Shared dispatcher with static executor service. */
+    private static final Dispatcher dispatcher;
+
+    static {
+        dispatcher = new Dispatcher(executionExecutor);
+        dispatcher.setMaxRequests(256);
+        dispatcher.setMaxRequestsPerHost(256);
+    }
+
+    /** Shared connection pool. */
+    private static final ConnectionPool connectionPool = new ConnectionPool(100, 10, TimeUnit.MINUTES);
+
     /**
      * The {@link ScheduledExecutorService} used for scheduling call retries. This thread pool is distinct from OkHttp's
      * internal thread pool and from the thread pool used by {@link #executionExecutor}.
@@ -151,10 +163,9 @@ public final class OkHttpClients {
         client.connectionSpecs(createConnectionSpecs(config.enableGcmCipherSuites()));
 
         // increase default connection pool from 5 @ 5 minutes to 100 @ 10 minutes
-        client.connectionPool(new ConnectionPool(100, 10, TimeUnit.MINUTES));
+        client.connectionPool(connectionPool);
 
-        // dispatcher with static executor service
-        client.dispatcher(createDispatcher());
+        client.dispatcher(dispatcher);
 
         return new RemotingOkHttpClient(
                 client.build(),
@@ -210,13 +221,6 @@ public final class OkHttpClients {
                                 : CipherSuites.fastCipherSuites())
                         .build(),
                 ConnectionSpec.CLEARTEXT);
-    }
-
-    private static Dispatcher createDispatcher() {
-        Dispatcher dispatcher = new Dispatcher(executionExecutor);
-        dispatcher.setMaxRequests(256);
-        dispatcher.setMaxRequestsPerHost(256);
-        return dispatcher;
     }
 
 }
