5f9a202467724bbe6ed52442311947098b48f49c
Joe Ellis <ellis125@gmail.com>
2018-09-14 17:56:14 +0100

    Stop propagating RemoteExceptions (#801)

--- conjure-java-jersey-server/src/test/java/com/palantir/conjure/java/server/jersey/ExceptionMappingTest.java
+++ conjure-java-jersey-server/src/test/java/com/palantir/conjure/java/server/jersey/ExceptionMappingTest.java
@@ -109,19 +109,20 @@ public final class ExceptionMappingTest {
     @Test
     public void testRemoteException() throws IOException {
         Response response = target.path("throw-remote-exception").request().get();
-        assertThat(response.getStatus(), is(REMOTE_EXCEPTION_STATUS_CODE));
+        assertThat(response.getStatus(), is(ErrorType.INTERNAL.httpErrorCode()));
         String body =
                 new String(ByteStreams.toByteArray(response.readEntity(InputStream.class)), StandardCharsets.UTF_8);
 
         SerializableError error = ObjectMappers.newClientObjectMapper().readValue(body, SerializableError.class);
-        assertThat(error.errorCode(), is("errorCode"));
-        assertThat(error.errorName(), is("errorName"));
+        assertThat(error.errorInstanceId(), is("errorInstanceId"));
+        assertThat(error.errorCode(), is(ErrorType.INTERNAL.code().toString()));
+        assertThat(error.errorName(), is(ErrorType.INTERNAL.name()));
     }
 
     @Test
     public void testServiceException() throws IOException {
         Response response = target.path("throw-service-exception").request().get();
-        assertThat(response.getStatus(), is(REMOTE_EXCEPTION_STATUS_CODE));
+        assertThat(response.getStatus(), is(ErrorType.INVALID_ARGUMENT.httpErrorCode()));
         String body =
                 new String(ByteStreams.toByteArray(response.readEntity(InputStream.class)), StandardCharsets.UTF_8);
 
@@ -188,6 +189,7 @@ public final class ExceptionMappingTest {
         @Override
         public String throwRemoteException() {
             throw new RemoteException(SerializableError.builder()
+                    .errorInstanceId("errorInstanceId")
                     .errorCode("errorCode")
                     .errorName("errorName")
                     .build(),
