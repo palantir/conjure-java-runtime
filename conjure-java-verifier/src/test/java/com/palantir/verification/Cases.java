cbe56b24beccb6e1a4a37cd8cb6dc06fd9227a21
Dan Fox <dfox@palantir.com>
2018-10-25 16:05:27 +0100

    Add Cases and ignored-test-cases.yml

---
+++ conjure-java-verifier/src/test/java/com/palantir/verification/Cases.java
@@ -0,0 +1,79 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.verification;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.palantir.conjure.verification.ClientTestCases;
+import com.palantir.conjure.verification.EndpointName;
+import com.palantir.conjure.verification.IgnoredClientTestCases;
+import com.palantir.conjure.verification.IgnoredTestCases;
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+import javax.annotation.Nullable;
+
+public final class Cases {
+
+    public static final ClientTestCases TEST_CASES = deserializeTestCases(
+            new File("build/test-cases/test-cases.json"));
+    private static final IgnoredClientTestCases IGNORED_TEST_CASES = deserializeIgnoredClientTestCases(
+            new File("src/test/resources/ignored-test-cases.yml"));
+
+    private Cases() {}
+
+    private static ClientTestCases deserializeTestCases(File file) {
+        try {
+            return new ObjectMapper()
+                    .registerModule(new Jdk8Module())
+                    .readValue(file, com.palantir.conjure.verification.TestCases.class).getClient();
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    String.format("Unable to read %s, you may need to run ./gradlew copyTestCases", file),
+                    e);
+        }
+    }
+
+    private static IgnoredClientTestCases deserializeIgnoredClientTestCases(File file) {
+        try {
+            return new ObjectMapper(new YAMLFactory())
+                    .registerModule(new Jdk8Module())
+                    .readValue(file, IgnoredTestCases.class).getClient();
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    String.format("Unable to read %s", file),
+                    e);
+        }
+    }
+
+    public static boolean shouldIgnore(EndpointName endpointName, String json) {
+        return setContains(IGNORED_TEST_CASES.getAutoDeserialize().get(endpointName), json)
+                || setContains(IGNORED_TEST_CASES.getSingleHeaderService().get(endpointName), json)
+                || setContains(IGNORED_TEST_CASES.getSinglePathParamService().get(endpointName), json)
+                || setContains(IGNORED_TEST_CASES.getSingleQueryParamService().get(endpointName), json);
+    }
+
+    private static boolean setContains(@Nullable Set<String> set, String item) {
+        if (set == null) {
+            return false;
+        }
+
+        return set.contains(item);
+    }
+}
