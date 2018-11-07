0081557e7695323bb36e4939d71d55a5fd59a702
Dan Fox <dfox@palantir.com>
2018-11-07 14:13:57 +0000

    Jackson204Decoder handles conjure aliases

---
+++ conjure-java-jaxrs-client/src/main/java/feign/Jackson204Decoder.java
@@ -0,0 +1,140 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package feign;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.palantir.logsafe.Preconditions;
+import com.palantir.logsafe.SafeArg;
+import feign.codec.Decoder;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Interprets HTTP 204 as an 'empty' type using Jackson initially, then using reflection
+ * to manually invoke a static factory annotated with {@link JsonCreator}.
+ *
+ * Empty instances are cached and re-used to avoid reflection and exceptions on a hot codepath.
+ */
+public final class Jackson204Decoder implements Decoder {
+
+    private final LoadingCache<Type, Object> blankInstanceCache;
+    private final Decoder delegate;
+
+    public Jackson204Decoder(Decoder delegate, ObjectMapper mapper) {
+        this.delegate = delegate;
+        this.blankInstanceCache = Caffeine.newBuilder()
+                .maximumSize(10_000)
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .build(new BlankInstanceLoader(mapper));
+    }
+
+    @Override
+    public Object decode(Response response, Type type) throws IOException {
+        if (response.status() == 204) {
+            @Nullable Object object = blankInstanceCache.get(type);
+            return Preconditions.checkNotNull(
+                    object,
+                    "Received HTTP 204 but unable to construct an empty instance for return type",
+                    SafeArg.of("type", type));
+        } else {
+            return delegate.decode(response, type);
+        }
+    }
+
+    private static class BlankInstanceLoader implements CacheLoader<Type, Object> {
+        private static final Logger log = LoggerFactory.getLogger(BlankInstanceLoader.class);
+        private final ObjectMapper mapper;
+
+        BlankInstanceLoader(ObjectMapper mapper) {
+            this.mapper = mapper;
+        }
+
+        @Nullable
+        @Override
+        public Object load(@Nonnull Type type) {
+            return constructInstanceOutOfThinAir(Types.getRawType(type))
+                    .orElse(null);
+        }
+
+        private Optional<Object> constructInstanceOutOfThinAir(Class<?> clazz) {
+            // this is our preferred way to construct instances
+            Optional<Object> jacksonInstance = jacksonDeserializeFromNull(clazz);
+            if (jacksonInstance.isPresent()) {
+                return jacksonInstance;
+            }
+
+            // fallback to manual reflection to handle aliases of optionals (and aliases of aliases of optionals)
+            Optional<Method> jsonCreator = getJsonCreatorStaticMethod(clazz);
+            if (jsonCreator.isPresent()) {
+                Method method = jsonCreator.get();
+                Class<?> parameterType = method.getParameters()[0].getType();
+                Optional<Object> parameter = constructInstanceOutOfThinAir(parameterType); // recurse!
+                // TODO(dfox): if someone has a recursive chain of factories, I think this is infinite :/
+
+                if (parameter.isPresent()) {
+                    return invokeStaticFactoryMethod(method, parameter.get());
+                } else {
+                    log.debug("Found a @JsonCreator, but couldn't figure out how to construct the argument");
+                    return Optional.empty();
+                }
+            }
+
+            log.debug("Jackson couldn't instantiate this and also couldn't find a usable @JsonCreator");
+            return Optional.empty();
+        }
+
+        private Optional<Object> jacksonDeserializeFromNull(Class<?> clazz) {
+            try {
+                return Optional.ofNullable(mapper.readValue("null", clazz));
+            } catch (IOException e) {
+                return Optional.empty();
+            }
+        }
+
+        // doesn't attempt to handle multiple @JsonCreator methods on one class
+        private static Optional<Method> getJsonCreatorStaticMethod(@Nonnull Class<?> clazz) {
+            return Arrays.stream(clazz.getMethods())
+                    .filter(method -> Modifier.isStatic(method.getModifiers())
+                            && method.getParameterCount() == 1
+                            && method.getAnnotation(JsonCreator.class) != null)
+                    .findFirst();
+        }
+
+        private static Optional<Object> invokeStaticFactoryMethod(Method method, Object parameter) {
+            try {
+                return Optional.ofNullable(method.invoke(null, parameter));
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                log.debug("Reflection instantiation failed", e);
+                return Optional.empty();
+            }
+        }
+    }
+}
---
+++ conjure-java-jaxrs-client/src/test/java/feign/Jackson204DecoderTest.java
@@ -0,0 +1,254 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package feign;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.net.HttpHeaders;
+import com.palantir.conjure.java.serialization.ObjectMappers;
+import feign.codec.Decoder;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+import javax.annotation.Generated;
+import javax.ws.rs.core.MediaType;
+import org.junit.Test;
+
+public class Jackson204DecoderTest {
+
+    private static final ObjectMapper mapper = ObjectMappers.newClientObjectMapper();
+    private static final Response HTTP_204 = Response.create(204, "No Content", Collections.emptyMap(), new byte[] {});
+    private final Decoder delegate = mock(Decoder.class);
+    private final Jackson204Decoder jackson204Decoder = new Jackson204Decoder(delegate, mapper);
+
+    @Test
+    public void http_200_uses_delegate_decoder() throws IOException {
+        Response http200 = Response.create(200, "OK", ImmutableMap.of(
+                HttpHeaders.CONTENT_TYPE,
+                ImmutableSet.of(MediaType.TEXT_PLAIN)), "text response", StandardCharsets.UTF_8);
+
+        jackson204Decoder.decode(http200, String.class);
+        verify(delegate, times(1)).decode(any(), any());
+    }
+
+    @Test
+    public void prove_jackson_doesnt_work_out_of_the_box() throws IOException {
+        assertThat(mapper.readValue("null", Alias1.class)).isNull(); // we want `Alias1.of(Optional.empty())`
+        assertThat(mapper.readValue("null", Alias3.class)).isNull();
+    }
+
+    @Test
+    public void capture_jackson_optional_handling_behaviour() throws IOException {
+        assertThat(mapper.readValue("null", com.google.common.base.Optional.class))
+                .isEqualTo(com.google.common.base.Optional.absent());
+        assertThat(mapper.readValue("null", OptionalInt.class)).isEqualTo(OptionalInt.empty());
+        assertThat(mapper.readValue("null", OptionalDouble.class)).isEqualTo(OptionalDouble.empty());
+        assertThat(mapper.readValue("null", OptionalLong.class)).isEqualTo(OptionalLong.empty());
+    }
+
+    @Test
+    public void http_204_turns_empty_body_into_alias_of_OptionalEmpty() throws IOException {
+        assertThat(jackson204Decoder.decode(HTTP_204, Alias1.class)).isEqualTo(Alias1.of(Optional.empty()));
+        verifyZeroInteractions(delegate);
+    }
+
+    @Test
+    public void http_204_turns_empty_body_into_alias_of_OptionalInt() throws IOException {
+        assertThat(jackson204Decoder.decode(HTTP_204, Alias2.class)).isEqualTo(Alias2.of(OptionalInt.empty()));
+    }
+
+    @Test
+    public void http_204_turns_empty_body_into_alias_of_OptionalDouble() throws IOException {
+        assertThat(jackson204Decoder.decode(HTTP_204, Alias3.class)).isEqualTo(Alias3.of(OptionalDouble.empty()));
+    }
+
+    @Test
+    public void http_204_can_handle_alias_of_alias_of_optional_string() throws IOException {
+        assertThat(jackson204Decoder.decode(HTTP_204, AliasAlias1.class))
+                .isEqualTo(AliasAlias1.of(Alias1.of(Optional.empty())));
+    }
+
+    @Test
+    public void http_204_reuses_the_same_instance_each_time() throws IOException {
+        Object first = jackson204Decoder.decode(HTTP_204, Alias1.class);
+        Object second = jackson204Decoder.decode(HTTP_204, Alias1.class);
+        assertThat(first).isSameAs(second);
+    }
+
+    @Generated("com.palantir.conjure.java.types.AliasGenerator")
+    private static final class Alias1 {
+        private final Optional<String> value;
+
+        private Alias1(Optional<String> value) {
+            Objects.requireNonNull(value, "value cannot be null");
+            this.value = value;
+        }
+
+        @JsonValue
+        public Optional<String> get() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return this == other
+                    || (other instanceof Alias1
+                    && this.value.equals(((Alias1) other).value));
+        }
+
+        @Override
+        public int hashCode() {
+            return value.hashCode();
+        }
+
+        @JsonCreator
+        public static Alias1 of(Optional<String> value) {
+            return new Alias1(value);
+        }
+    }
+
+    @Generated("com.palantir.conjure.java.types.AliasGenerator")
+    private static final class Alias2 {
+        private final OptionalInt value;
+
+        private Alias2(OptionalInt value) {
+            Objects.requireNonNull(value, "value cannot be null");
+            this.value = value;
+        }
+
+        @JsonValue
+        public OptionalInt get() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return this == other
+                    || (other instanceof Alias2
+                    && this.value.equals(((Alias2) other).value));
+        }
+
+        @Override
+        public int hashCode() {
+            return value.hashCode();
+        }
+
+        @JsonCreator
+        public static Alias2 of(OptionalInt value) {
+            return new Alias2(value);
+        }
+    }
+
+    @Generated("com.palantir.conjure.java.types.AliasGenerator")
+    private static final class Alias3 {
+        private final OptionalDouble value;
+
+        private Alias3(OptionalDouble value) {
+            Objects.requireNonNull(value, "value cannot be null");
+            this.value = value;
+        }
+
+        @JsonValue
+        public OptionalDouble get() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return this == other
+                    || (other instanceof Alias3
+                    && this.value.equals(((Alias3) other).value));
+        }
+
+        @Override
+        public int hashCode() {
+            return value.hashCode();
+        }
+
+        @JsonCreator
+        public static Alias3 of(OptionalDouble value) {
+            return new Alias3(value);
+        }
+    }
+
+    @Generated("com.palantir.conjure.java.types.AliasGenerator")
+    private static final class AliasAlias1 {
+        private final Alias1 value;
+
+        private AliasAlias1(Alias1 value) {
+            Objects.requireNonNull(value, "value cannot be null");
+            this.value = value;
+        }
+
+        @JsonValue
+        public Alias1 get() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return this == other
+                    || (other instanceof AliasAlias1
+                    && this.value.equals(((AliasAlias1) other).value));
+        }
+
+        @Override
+        public int hashCode() {
+            return value.hashCode();
+        }
+
+        @JsonCreator
+        public static AliasAlias1 of(Alias1 value) {
+            return new AliasAlias1(value);
+        }
+    }
+}
