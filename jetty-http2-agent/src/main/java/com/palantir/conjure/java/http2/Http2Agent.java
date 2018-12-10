6f2d53d9deb00ce8eb84802808c5c1bf4e402e41
Mark Elliot <melliot@palantir.com>
2018-12-10 19:13:01 +0000

    Disable automatic agent-based ALPN installation
    Callers should remove `Http2Agent.install()` invocations and may still
    obtain http2 based connections using conscrypt and/or undertow instead.

--- jetty-http2-agent/src/main/java/com/palantir/conjure/java/http2/Http2Agent.java
+++ jetty-http2-agent/src/main/java/com/palantir/conjure/java/http2/Http2Agent.java
@@ -16,41 +16,22 @@
 
 package com.palantir.conjure.java.http2;
 
-import com.ea.agentloader.AgentLoader;
 import java.lang.instrument.Instrumentation;
-import org.mortbay.jetty.alpn.agent.Premain;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-/** A very simple wrapper around the jetty-alpn-agent for dynamic loading. */
+/** Deprecated: this class no longer has any effect. */
+@Deprecated
 public final class Http2Agent {
     private Http2Agent() {}
 
-    private static final Logger log = LoggerFactory.getLogger(Http2Agent.class);
-
-    private static boolean hasBeenInstalled = false;
-
-    /**
-     * Installs the jetty-alpn-agent dynamically.
-     * <p>
-     * This method protects itself from multiple invocations so may be called in multiple places, but will only
-     * ever invoke the installation once.
-     */
+    /** Deprecated: this method no longer has any effect and is safe to stop calling. */
+    @Deprecated
     public static synchronized void install() {
-        if (hasBeenInstalled) {
-            return;
-        }
-
-        try {
-            AgentLoader.loadAgentClass(Http2Agent.class.getName(), "");
-            hasBeenInstalled = true;
-        } catch (Exception e) {
-            log.warn("Unable to dynamically install jetty-alpn-agent via ea-agent-loader, proceeding anyway...", e);
-        }
+        // no-op
     }
 
-    /** Agent entry-point. */
+    /** Deprecated: this method no longer has any effect and is safe to stop calling. */
+    @Deprecated
     public static void agentmain(String args, Instrumentation inst) throws Exception {
-        Premain.premain(args, inst);
+        // no-op
     }
 }
