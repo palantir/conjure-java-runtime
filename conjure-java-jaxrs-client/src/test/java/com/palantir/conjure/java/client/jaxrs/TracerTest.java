e176aebf3f8b68f56918763df866b8d9f7f63a19
James Baker <jbaker@palantir.com>
2019-03-18 13:16:58 -0400

    Failing test

--- conjure-java-jaxrs-client/src/test/java/com/palantir/conjure/java/client/jaxrs/TracerTest.java
+++ conjure-java-jaxrs-client/src/test/java/com/palantir/conjure/java/client/jaxrs/TracerTest.java
@@ -17,15 +17,22 @@
 package com.palantir.conjure.java.client.jaxrs;
 
 import static org.hamcrest.Matchers.either;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
+import com.google.common.collect.Sets;
 import com.palantir.conjure.java.okhttp.HostMetricsRegistry;
 import com.palantir.tracing.Tracer;
 import com.palantir.tracing.api.OpenSpan;
 import com.palantir.tracing.api.TraceHttpHeaders;
 import java.io.IOException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
@@ -44,11 +51,11 @@ public final class TracerTest extends TestBase {
     public void before() {
         String uri = "http://localhost:" + server.getPort();
         service = JaxRsClient.create(TestService.class, AGENT, new HostMetricsRegistry(), createTestConfig(uri));
-        server.enqueue(new MockResponse().setBody("\"server\""));
     }
 
     @Test
     public void testClientIsInstrumentedWithTracer() throws InterruptedException, IOException {
+        server.enqueue(new MockResponse().setBody("\"server\""));
         OpenSpan parentTrace = Tracer.startSpan("");
 
         Tracer.subscribe(TracerTest.class.getName(), span -> {
@@ -63,5 +70,57 @@ public final class TracerTest extends TestBase {
         RecordedRequest request = server.takeRequest();
         assertThat(request.getHeader(TraceHttpHeaders.TRACE_ID), is(traceId));
         assertThat(request.getHeader(TraceHttpHeaders.SPAN_ID), is(not(parentTrace.getSpanId())));
+    }
+
+    @Test
+    public void testLimiterAcquisitionMultiThread() {
+        reduceConcurrencyLimitTo1();
+        Set<String> observedTraceIds = Sets.newConcurrentHashSet();
+        addTraceSubscriber(observedTraceIds);
+        runTwoRequestsInParallel();
+        removeTraceSubscriber();
+        assertThat(observedTraceIds, hasSize(2));
+    }
+
+    private void runTwoRequestsInParallel() {
+        // time based delays are sad, but meh.
+        server.enqueue(new MockResponse().setBody("\"server\"").setHeadersDelay(100, TimeUnit.MILLISECONDS));
+        server.enqueue(new MockResponse().setBody("\"server\""));
+
+        CompletableFuture<?> first = CompletableFuture.runAsync(() -> {
+            Tracer.initTrace(Optional.of(true), "first");
+            Tracer.startSpan("");
+            service.string();
+        });
+        CompletableFuture<?> second = CompletableFuture.runAsync(() -> {
+            Tracer.initTrace(Optional.of(true), "second");
+            Tracer.startSpan("");
+            service.string();
+        });
+        first.join();
+        second.join();
+    }
+
+    private void addTraceSubscriber(Set<String> observedTraceIds) {
+        Tracer.subscribe(TracerTest.class.getName(), span -> {
+            if (span.getOperation().equals("GET /string")) {
+                observedTraceIds.add(span.getTraceId());
+            }
+        });
+    }
+
+    private void removeTraceSubscriber() {
+        Tracer.unsubscribe(TracerTest.class.getName());
+    }
+
+    private void reduceConcurrencyLimitTo1() {
+        int experimentallyThisNeedsToHappen3Times = 3;
+        for (int i = 0; i < experimentallyThisNeedsToHappen3Times; i++) {
+            IntStream.range(0, 3).forEach(x -> {
+                server.enqueue(new MockResponse().setResponseCode(429));
+            });
+            server.enqueue(new MockResponse().setBody("\"server\""));
+            assertThat(service.string(), is("server"));
+        }
     }
 }
