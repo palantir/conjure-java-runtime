52ed18d277178da24e4b4f86bf79bd3823d71204
Carter Kozak <ckozak@palantir.com>
2019-03-19 19:38:05 -0700

    fix expected exception type

--- conjure-java-jackson-serialization/src/test/java/com/palantir/conjure/java/serialization/ObjectMappersTest.java
+++ conjure-java-jackson-serialization/src/test/java/com/palantir/conjure/java/serialization/ObjectMappersTest.java
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -206,8 +207,8 @@ public final class ObjectMappersTest {
     public void testOptionalLongAsStringOverflowDeserialization() {
         BigInteger large = BigInteger.valueOf(Long.MAX_VALUE).add(BigInteger.ONE);
         assertThatThrownBy(() -> MAPPER.readValue("\"" + large + "\"", OptionalLong.class))
-                .isInstanceOf(JsonParseException.class)
-                .hasMessageContaining("out of range of long");
+                .isInstanceOf(InvalidFormatException.class)
+                .hasMessageContaining("not a valid long value");
     }
 
     @Test
