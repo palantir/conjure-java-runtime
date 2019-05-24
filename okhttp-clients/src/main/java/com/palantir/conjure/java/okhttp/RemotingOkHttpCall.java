4a1de4d8b9a79dbab169376f2b65729d2528468f
Paul Nepywoda <pnepywoda@palantir.com>
2019-05-24 13:08:26 -0700

    log hosts on retry

--- okhttp-clients/src/main/java/com/palantir/conjure/java/okhttp/RemotingOkHttpCall.java
+++ okhttp-clients/src/main/java/com/palantir/conjure/java/okhttp/RemotingOkHttpCall.java
@@ -207,6 +207,8 @@ final class RemotingOkHttpCall extends ForwardingCall {
 
                 log.info("Retrying call after failure",
                         SafeArg.of("backoffMillis", backoff.get().toMillis()),
+                        SafeArg.of("originalHost", request().url().host()),
+                        SafeArg.of("redirectToHost", redirectTo.get().host()),
                         UnsafeArg.of("redirectToUrl", redirectTo.get()),
                         exception);
                 Request redirectedRequest = request().newBuilder()
