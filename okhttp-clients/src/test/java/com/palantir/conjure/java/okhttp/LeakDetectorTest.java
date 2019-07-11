1ac915c29037c1307e604669203bfc179461fd1d
James Baker <jbaker@palantir.com>
2019-07-11 19:08:27 +0100

    Fix import

--- okhttp-clients/src/test/java/com/palantir/conjure/java/okhttp/LeakDetectorTest.java
+++ okhttp-clients/src/test/java/com/palantir/conjure/java/okhttp/LeakDetectorTest.java
@@ -16,7 +16,7 @@
 
 package com.palantir.conjure.java.okhttp;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
