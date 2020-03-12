a3abc20dbcda88f2738fb360df5f07a2e6f22576
James Baker <jbaker@palantir.com>
2020-03-12 19:10:38 +0000

    Don't share connection pools
    Internally, we've seen a number of issues that appear to be related to connection pool reuse between clients. What we think we see is that a client is hanging onto a connection (which after some period is actually dead, so holding no resources), but the ConnectionPool itself manages a deque of connections, which it synchronizes access too. After long enough runtime, this queue becomes so long that accessing it times out.
    
    Now, the curious thing here is that this connection pool reuse is totally pointless! Reuse relies on the respective connections having the same SSLSocketFactory. And, the way we construct clients internally, we _always_ use a new one each time - because we read the truststore file from disk.

--- okhttp-clients/src/main/java/com/palantir/conjure/java/okhttp/OkHttpClients.java
+++ okhttp-clients/src/main/java/com/palantir/conjure/java/okhttp/OkHttpClients.java
@@ -90,15 +90,6 @@ public final class OkHttpClients {
     /** Shared dispatcher with static executor service. */
     private static final Dispatcher dispatcher;
 
-    /** Shared connection pool. */
-    private static final ConnectionPool connectionPool = new ConnectionPool(
-            1000,
-            // Most servers use a one minute keepalive for idle connections, by using a shorter keepalive on
-            // clients we can avoid race conditions where the attempts to reuse a connection as the server
-            // closes it, resulting in unnecessary I/O exceptions and retrial.
-            55,
-            TimeUnit.SECONDS);
-
     private static DispatcherMetricSet dispatcherMetricSet;
 
     static {
@@ -274,9 +265,8 @@ public final class OkHttpClients {
         if (!config.enableGcmCipherSuites() || !config.enableHttp2().orElse(DEFAULT_ENABLE_HTTP2)) {
             client.protocols(ImmutableList.of(Protocol.HTTP_1_1));
         }
-
-        // increase default connection pool from 5 @ 5 minutes to 100 @ 10 minutes
-        client.connectionPool(connectionPool);
+        
+        client.connectionPool(newConnectionPool());
 
         client.dispatcher(dispatcher);
 
@@ -337,5 +327,15 @@ public final class OkHttpClients {
                                         : CipherSuites.fastCipherSuites())
                         .build(),
                 ConnectionSpec.CLEARTEXT);
+    }
+    
+    private static ConnectionPool newConnectionPool() {
+        return new ConnectionPool(
+            1000,
+            // Most servers use a one minute keepalive for idle connections, by using a shorter keepalive on
+            // clients we can avoid race conditions where the attempts to reuse a connection as the server
+            // closes it, resulting in unnecessary I/O exceptions and retrial.
+            55,
+            TimeUnit.SECONDS);
     }
 }
