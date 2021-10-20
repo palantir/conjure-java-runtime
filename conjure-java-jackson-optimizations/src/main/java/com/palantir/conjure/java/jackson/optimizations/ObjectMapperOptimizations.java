c0a7e21a7eb4859c922c2583cc38d4ed9c453f3e
Carter Kozak <ckozak@palantir.com>
2021-10-20 17:51:31 -0400

    cleaner impl

--- conjure-java-jackson-optimizations/src/main/java/com/palantir/conjure/java/jackson/optimizations/ObjectMapperOptimizations.java
+++ conjure-java-jackson-optimizations/src/main/java/com/palantir/conjure/java/jackson/optimizations/ObjectMapperOptimizations.java
@@ -27,20 +27,29 @@ import java.util.List;
 public final class ObjectMapperOptimizations {
 
     private static final boolean NO_OPTIMIZATIONS =
-            // These optimizations are not used within graalvm because they leverage dynamic class creation
-            // The nativeimage check should not be refactored into a utility method which may reduce our ability to
+            // These optimizations are not used within graalvm because they leverage dynamic class creation The
+            // nativeimage check should not be refactored into a utility method which may reduce our ability to
             // tree-shake.
             System.getProperty("org.graalvm.nativeimage.imagecode") != null
                     // This may be globally configured with a system property
-                    // We disable afterburner optimizations by default on java 16+ where internal access is
-                    // restricted by https://openjdk.java.net/jeps/396 and https://openjdk.java.net/jeps/403
-                    || Boolean.parseBoolean(System.getProperty(
-                            "com.palantir.conjure.java.jackson.optimizations.disabled",
-                            Runtime.version().feature() >= 16 ? "true" : "false"));
+                    || readProperty(
+                            "com.palantir.conjure.java.jackson.optimizations.disabled", shouldDisableByDefault());
 
     public static List<? extends com.fasterxml.jackson.databind.Module> createModules() {
         return NO_OPTIMIZATIONS ? List.of() : List.<com.fasterxml.jackson.databind.Module>of(new AfterburnerModule());
     }
 
     private ObjectMapperOptimizations() {}
+
+    /**
+     * We disable afterburner optimizations by default on java 16+ where internal access is
+     * restricted by https://openjdk.java.net/jeps/396 and https://openjdk.java.net/jeps/403.
+     */
+    private static boolean shouldDisableByDefault() {
+        return Runtime.version().feature() >= 16;
+    }
+
+    private static boolean readProperty(String property, boolean defaultValue) {
+        return Boolean.parseBoolean(System.getProperty(property, Boolean.toString(defaultValue)));
+    }
 }
