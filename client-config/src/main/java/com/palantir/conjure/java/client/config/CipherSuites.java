a581cdde609c6d0b028e47ebb438a8de7776445b
Carter Kozak <ckozak@palantir.com>
2022-02-17 11:07:12 -0500

    Expand the supported cipher suite list
    Includes the following new suites:
    - `TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384`
    - `TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256`
    - `TLS_DHE_RSA_WITH_AES_256_GCM_SHA384`
    - `TLS_DHE_RSA_WITH_AES_128_GCM_SHA256`

--- client-config/src/main/java/com/palantir/conjure/java/client/config/CipherSuites.java
+++ client-config/src/main/java/com/palantir/conjure/java/client/config/CipherSuites.java
@@ -34,12 +34,16 @@ public final class CipherSuites {
     private static final ImmutableList<String> GCM_CIPHER_SUITES = ImmutableList.of(
             "TLS_AES_256_GCM_SHA384",
             "TLS_AES_128_GCM_SHA256",
+            "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+            "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
             "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
             "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
             "TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384",
             "TLS_ECDH_RSA_WITH_AES_128_GCM_SHA256",
             "TLS_RSA_WITH_AES_256_GCM_SHA384",
-            "TLS_RSA_WITH_AES_128_GCM_SHA256");
+            "TLS_RSA_WITH_AES_128_GCM_SHA256",
+            "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+            "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256");
 
     private static final ImmutableList<String> ALL_CIPHER_SUITES = ImmutableList.<String>builder()
             .addAll(GCM_CIPHER_SUITES)
