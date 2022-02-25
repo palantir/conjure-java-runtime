927a8b19c7cad583e4cd07954ceccb348eef192d
Carter Kozak <ckozak@palantir.com>
2022-02-25 15:47:10 -0500

    Expose deprecated cipher info (#2272)
    Expose deprecated cipher info

--- client-config/src/main/java/com/palantir/conjure/java/client/config/CipherSuites.java
+++ client-config/src/main/java/com/palantir/conjure/java/client/config/CipherSuites.java
@@ -17,37 +17,40 @@
 package com.palantir.conjure.java.client.config;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import java.util.Set;
 
 public final class CipherSuites {
 
-    private static final ImmutableList<String> OTHER_CIPHER_SUITES = ImmutableList.of(
-            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
-            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
-            "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA384",
-            "TLS_ECDH_RSA_WITH_AES_128_CBC_SHA256",
-            "TLS_RSA_WITH_AES_128_CBC_SHA256",
-            "TLS_RSA_WITH_AES_256_CBC_SHA256",
-            "TLS_CHACHA20_POLY1305_SHA256",
-            "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
-            "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256");
-
-    private static final ImmutableList<String> GCM_CIPHER_SUITES = ImmutableList.of(
+    private static final ImmutableList<String> CIPHERS = ImmutableList.of(
             "TLS_AES_256_GCM_SHA384",
             "TLS_AES_128_GCM_SHA256",
             "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
             "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
             "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+            // required to include for http/2: https://http2.github.io/http2-spec/#rfc.section.9.2.2
             "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+            "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+            "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
+            "TLS_CHACHA20_POLY1305_SHA256",
+            "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+            "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256");
+
+    private static final ImmutableSet<String> DEPRECATED_CIPHERS = ImmutableSet.of(
             "TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384",
             "TLS_ECDH_RSA_WITH_AES_128_GCM_SHA256",
             "TLS_RSA_WITH_AES_256_GCM_SHA384",
             "TLS_RSA_WITH_AES_128_GCM_SHA256",
-            "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
-            "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256");
+            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
+            "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA384",
+            "TLS_ECDH_RSA_WITH_AES_128_CBC_SHA256",
+            "TLS_RSA_WITH_AES_128_CBC_SHA256",
+            "TLS_RSA_WITH_AES_256_CBC_SHA256");
 
     private static final ImmutableList<String> ALL_CIPHER_SUITES = ImmutableList.<String>builder()
-            .addAll(GCM_CIPHER_SUITES)
-            .addAll(OTHER_CIPHER_SUITES)
+            .addAll(CIPHERS)
+            .addAll(DEPRECATED_CIPHERS)
             .build();
 
     /**
@@ -63,14 +66,26 @@ public final class CipherSuites {
         return allCipherSuites();
     }
 
-    /** Known safe GCM cipher suites. */
+    /**
+     * GCM cipher suites.
+     *
+     * @deprecated should not be used, this is an artifact from java-8 when gcm ciphers performed poorly.
+     */
+    @Deprecated
     public static String[] gcmCipherSuites() {
-        return GCM_CIPHER_SUITES.toArray(new String[0]);
+        return ALL_CIPHER_SUITES.stream()
+                .filter(cipherString -> cipherString.contains("_GCM_"))
+                .toArray(String[]::new);
     }
 
     /** Union of {@link #fastCipherSuites()} and {@link #gcmCipherSuites()}. */
     public static String[] allCipherSuites() {
         return ALL_CIPHER_SUITES.toArray(new String[0]);
+    }
+
+    /** Returns ciphers which are currently supported, but are deprecated for removal in a future release. */
+    public static Set<String> deprecatedCiphers() {
+        return DEPRECATED_CIPHERS;
     }
 
     private CipherSuites() {}
