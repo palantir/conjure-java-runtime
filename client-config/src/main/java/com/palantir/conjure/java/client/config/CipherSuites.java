cb6371a54a2d1fe13b6344844dfb9225b91a9f23
Carter Kozak <ckozak@palantir.com>
2022-10-21 13:35:53 -0700

    Deprecate less secure ciphers using classic Diffie-Hellman key exchange (DHE)

--- client-config/src/main/java/com/palantir/conjure/java/client/config/CipherSuites.java
+++ client-config/src/main/java/com/palantir/conjure/java/client/config/CipherSuites.java
@@ -30,13 +30,13 @@ public final class CipherSuites {
             "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
             // required to include for http/2: https://http2.github.io/http2-spec/#rfc.section.9.2.2
             "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-            "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
-            "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
             "TLS_CHACHA20_POLY1305_SHA256",
             "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
             "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256");
 
     private static final ImmutableSet<String> DEPRECATED_CIPHERS = ImmutableSet.of(
+            "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+            "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
             "TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384",
             "TLS_ECDH_RSA_WITH_AES_128_GCM_SHA256",
             "TLS_RSA_WITH_AES_256_GCM_SHA384",
