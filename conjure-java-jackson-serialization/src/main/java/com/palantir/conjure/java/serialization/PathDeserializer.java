280fb52b6d17a97c134e16f54fd878c0c060d64c
David Schlosnagle <davids@palantir.com>
2023-10-21 17:22:17 -0400

    SafeJsonMappingException

--- conjure-java-jackson-serialization/src/main/java/com/palantir/conjure/java/serialization/PathDeserializer.java
+++ conjure-java-jackson-serialization/src/main/java/com/palantir/conjure/java/serialization/PathDeserializer.java
@@ -37,11 +37,15 @@ package com.palantir.conjure.java.serialization;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
-import com.palantir.logsafe.exceptions.SafeIoException;
+import com.google.errorprone.annotations.CompileTimeConstant;
+import com.palantir.logsafe.Arg;
+import com.palantir.logsafe.SafeLoggable;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public final class PathDeserializer extends StdScalarDeserializer<Path> {
     private static final long serialVersionUID = 1;
@@ -59,7 +63,26 @@ public final class PathDeserializer extends StdScalarDeserializer<Path> {
             }
             // 16-Oct-2015: should we perhaps allow JSON Arrays (of Strings) as well?
         }
-        throw new SafeIoException(
-                "Could not deserialize path", ctxt.wrongTokenException(parser, Path.class, token, null));
+        throw new SafeJsonMappingException(
+                "Could not deserialize path", parser, ctxt.wrongTokenException(parser, Path.class, token, null));
+    }
+
+    private static final class SafeJsonMappingException extends JsonMappingException implements SafeLoggable {
+        private final String logMessage;
+
+        SafeJsonMappingException(@CompileTimeConstant String message, JsonParser parser, JsonMappingException cause) {
+            super(parser, message, cause);
+            this.logMessage = message;
+        }
+
+        @Override
+        public String getLogMessage() {
+            return logMessage;
+        }
+
+        @Override
+        public List<Arg<?>> getArgs() {
+            return List.of();
+        }
     }
 }
