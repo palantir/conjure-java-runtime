d8d2a9c36490bb3ce2f44b43f1b83c867daba4b9
Parker Winchester <pwinchester@palantir.com>
2026-05-28 16:34:09 -0400

    Enable Jackson Blackbird module

--- conjure-java-jackson-optimizations/src/main/java/com/palantir/conjure/java/jackson/optimizations/ObjectMapperOptimizations.java
+++ conjure-java-jackson-optimizations/src/main/java/com/palantir/conjure/java/jackson/optimizations/ObjectMapperOptimizations.java
@@ -16,6 +16,7 @@
 
 package com.palantir.conjure.java.jackson.optimizations;
 
+import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
 import java.util.List;
 
 /**
@@ -25,11 +26,15 @@ import java.util.List;
  */
 public final class ObjectMapperOptimizations {
 
+    private static final boolean NO_OPTIMIZATIONS =
+            // Blackbird generates accessor classes via LambdaMetafactory at runtime, which is incompatible with
+            // graalvm native-image. The nativeimage check should not be refactored into a utility method which may
+            // reduce our ability to tree-shake.
+            System.getProperty("org.graalvm.nativeimage.imagecode") != null
+                    || Boolean.getBoolean("com.palantir.conjure.java.jackson.optimizations.disabled");
+
     public static List<? extends com.fasterxml.jackson.databind.Module> createModules() {
-        // At one point this conditionally returned List.of(new AfterburnerModule), however afterburner is not
-        // supported on any supported LTS Java release anymore, and the Blackbird alternative incurs a memory
-        // leak (https://github.com/FasterXML/jackson-modules-base/issues/147).
-        return List.of();
+        return NO_OPTIMIZATIONS ? List.of() : List.of(new BlackbirdModule());
     }
 
     private ObjectMapperOptimizations() {}
